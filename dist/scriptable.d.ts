2ce958ad8439d0888b85d19d0338ca0bc60abf4e
schl3ck <42554556+schl3ck@users.noreply.github.com>
2020-03-06 13:09:43 +0100

    Updated version in output

--- dist/scriptable.d.ts
+++ dist/scriptable.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for iOS-Scriptable 1.3
+// Type definitions for iOS-Scriptable 1.4
 // Project: https://scriptable.app/
 // Definitions by: schl3ck <https://github.com/schl3ck>
 
--- template.d.ts
+++ template.d.ts
@@ -1,3 +1,3 @@
-// Type definitions for iOS-Scriptable 1.3
+// Type definitions for iOS-Scriptable 1.4
 // Project: https://scriptable.app/
 // Definitions by: schl3ck <https://github.com/schl3ck>
