8ad1dab664fea7854018a49a8439d1a904895b7a
schl3ck <schleck2@gmx.at>
2020-09-17 21:26:58 +0200

    Update for Scriptable version 1.5.0

--- template.d.ts
+++ template.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package scriptable-ios 1.4
+// Type definitions for non-npm package scriptable-ios 1.5
 // Project: https://scriptable.app/
 // Definitions by: schl3ck <https://github.com/schl3ck>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
