bc75ddc42fc69e59880ebe92a3eecfd930dec71b
schl3ck <schleck2@gmx.at>
2020-11-05 22:49:37 +0100

    Add @FuJuntao as owner to aid in PRs for DefinitelyTyped

--- template.d.ts
+++ template.d.ts
@@ -1,4 +1,5 @@
 // Type definitions for non-npm package scriptable-ios 1.5.1
 // Project: https://scriptable.app/
 // Definitions by: schl3ck <https://github.com/schl3ck>
+//                 FuJuntao <https://github.com/FuJuntao>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
