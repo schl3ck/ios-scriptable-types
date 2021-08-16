a4dde7e73dfb65cef841f0a3ffa28f62e0706da6
schl3ck <schleck2@gmx.at>
2021-08-16 20:45:13 +0200

    add FifiTheBulldog as author for helping with DefinitelyTyped PRs

--- template.d.ts
+++ template.d.ts
@@ -2,4 +2,5 @@
 // Project: https://scriptable.app/
 // Definitions by: schl3ck <https://github.com/schl3ck>
 //                 FuJuntao <https://github.com/FuJuntao>
+//                 FifiTheBulldog <https://github.com/FifiTheBulldog>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
