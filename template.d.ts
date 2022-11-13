b99a35809bd6fd9e004e20bb408219adb127cb70
schl3ck <schleck2@gmx.at>
2022-11-13 18:02:20 +0100

    update for Scriptable 1.7 and use ReadonlyArray inside function definitions

--- template.d.ts
+++ template.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for non-npm package scriptable-ios 1.6
+// Type definitions for non-npm package scriptable-ios 1.7
 // Project: https://scriptable.app/
 // Definitions by: schl3ck <https://github.com/schl3ck>
 //                 FuJuntao <https://github.com/FuJuntao>
 //                 FifiTheBulldog <https://github.com/FifiTheBulldog>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 4.7
